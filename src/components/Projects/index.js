65d5e75cffc199af64d3662fb06b7004cfad60d1
cdtest <corydavis1989@gmail.com>
2022-04-21 16:00:26 -0500

    added projects and about components/images

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import "./Projects.css";
+import PenguinPost from "../../assets/images/Penguin-post-login.png";
+import RioColo from '../../assets/images/riocolo-cup.jpg'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="container">
+      <h1 className="projects_title">PROJECTS</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <a href="https://github.com/cmd0160/penguin-post">
+            <img
+              src={RioColo}
+              alt="Penguin Post Login Page"
+              className="penguinPost"
+            />
+          </a>
+        </div>
+        <div className="col-md-6">
+        <a href="https://github.com/cmd0160/penguin-post">
+            <img
+              src={PenguinPost}
+              alt="Putting green sunrise"
+              className="penguinPost"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6">
+        <a href="">
+            <img
+              src={PenguinPost}
+              alt="Penguin Post Login Page"
+              className="penguinPost"
+            />
+          </a>
+        </div>
+        <div className="col-md-6">
+        <a href="">
+            <img
+              src={PenguinPost}
+              alt="Penguin Post Login Page"
+              className="penguinPost"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6">
+        <a href="">
+            <img
+              src={PenguinPost}
+              alt="Penguin Post Login Page"
+              className="penguinPost"
+            />
+          </a>
+        </div>
+        <div className="col-md-6">
+        <a href="">
+            <img
+              src={PenguinPost}
+              alt="Penguin Post Login Page"
+              className="penguinPost"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
