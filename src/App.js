5918386198feb8505fc3ab7c62b16cdd54c720a8
cdtest <corydavis1989@gmail.com>
2022-04-20 19:05:47 -0500

    Added Home Component and Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './App.css'
+import Home from './components/Home.js'
 import Nav from './components/Nav'
+import Footer from './components/Footer'
 
 
 const App = () => {
   return (
     <div>
       <Nav />
+      <main>
+        <Home />
+      </main>
+      <Footer />
     </div>
   )
 }
