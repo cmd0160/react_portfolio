61c47e574531796221200a1f01556e6b8ef41d3c
cdtest <corydavis1989@gmail.com>
2022-04-21 13:16:21 -0500

    added social media icons and dialed in positioning

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import linkedIn from '../../assets/images/LinkedIn_icon.png'
-import gitHub from '../../assets/images/GitHub_icon.png'
+import React from "react";
+import linkedIn from "../../assets/images/LinkedIn_icon.png";
+import gitHub from "../../assets/images/GitHub_icon.png";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className='container gh_li_con'>
-      <div className="icons">
-        <img src={gitHub} alt="GitHub Icon" className="github" />
-      </div>
-      <div className="icons">
-        <img src={linkedIn} alt="GitHub Icon" className="github" />
+    <div className="container footer">
+      <div className="row">
+        <div className="col-md-6 gh_icon">
+          <img src={gitHub} alt="GitHub Icon" className="github" />
+        </div>
+        <div className="col-md-6 li_icon">
+          <img src={linkedIn} alt="LinkedIn Icon" className="linkedin" />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
