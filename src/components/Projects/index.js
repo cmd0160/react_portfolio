a8838fb03aacfe1ab87351aa2064047f89b15e53
cdtest <corydavis1989@gmail.com>
2022-05-10 11:01:39 -0500

    nicer styling to porjects page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,50 +1,51 @@
 import React, { useState } from "react";
 import "./Projects.css";
 import { images } from "../../utils/carouselImages";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faSquareCaretLeft,
-  faSquareCaretRight,
-} from "@fortawesome/free-solid-svg-icons";
-
-const leftArrow = <FontAwesomeIcon icon={faSquareCaretLeft} size="2x" />;
-const rightArrow = <FontAwesomeIcon icon={faSquareCaretRight} size="2x" />;
 
 const Projects = () => {
   const [currentImage, setCurrentImage] = useState(0);
 
   return (
-    <>
-      <h1 className="projects_title">Projects</h1>
-      <div className="carousel">
-        <div
-          className="carouselInner"
-          style={{ backgroundImage: `url(${images[currentImage].img})` }}
-        >
-          <div
-            className="left"
-            onClick={() => {
-              currentImage > 0 && setCurrentImage(currentImage - 1);
-            }}
-          >
-            {leftArrow}
+    <div className="container">
+      <div className="row">
+        <div className="carousel_con col-lg-6">
+          <h3 className="projects_title">Projects</h3>
+          <div className="carousel">
+            <div
+              className="carouselInner"
+              style={{ backgroundImage: `url(${images[currentImage].img})` }}
+            ></div>
           </div>
+          <div className="btn_con">
+            <button
+              className="btn last"
+              onClick={() => {
+                currentImage > 0 && setCurrentImage(currentImage - 1);
+              }}
+            >
+              last
+            </button>
+            <button
+              className="btn next"
+              onClick={() => {
+                currentImage < images.length - 1 &&
+                  setCurrentImage(currentImage + 1);
+              }}
+            >
+              next
+            </button>
+          </div>
+        </div>
+
+        <div className="project_text col-lg-6">
           <div className="center">
             <h1>{images[currentImage].title}</h1>
             <p>{images[currentImage].description}</p>
-          </div>
-          <div
-            className="right"
-            onClick={() => {
-              currentImage < images.length - 1 &&
-                setCurrentImage(currentImage + 1);
-            }}
-          >
-            {rightArrow}
+            <button className="btn">View Project</button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
