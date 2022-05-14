640744456f23a87425ce027cd0bc9219b272b1a0
cdtest <corydavis1989@gmail.com>
2022-05-14 17:08:57 -0500

    rio colorado project info

--- src/utils/carouselImages.js
+++ src/utils/carouselImages.js
@@ -12,8 +12,12 @@ export const images = [
     img: penguin,
   },
   {
-    title: "Riocolo",
-    description: "This is a description of riocolo",
+    title: "Rio Colorado Golf Course",
+    description:
+      "This project featured a full-scale rebuild of the Rio Colorado Golf Course's website, starting completely from a blank screen on both the design and code. Taking the site from the 1990's into the modern era, it has been future-proofed with technologies such as responsive design, React, MongoDB, and GraphQL. It has features such as a full course page with hole selector tool, user login/logout authentication containing Admin-level privileges that includes easy price customization, and queries/mutations for full CRUD capabilities.",
+      tech: "HTML, CSS, Javscript, Apollo Server/Client, Bootstrap, Express, Framer Motion, GraphQL, Heroku, MongoDB, Mongoose, Node.js, React",
+      url: "https://github.com/cmd0160/UTA-Final-Project",
+      deployed_site: "https://www.riocoloradogolf.com/",
     img: ricolo,
   },
 ];
